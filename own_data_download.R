62471a9839609c04bb2db433e5e4e546e9469f1c
invisiblegoldfish <46575856+invisiblegoldfish@users.noreply.github.com>
2023-11-07 17:12:49 +0100

    deleted credentials

--- own_data_download.R
+++ own_data_download.R
@@ -1,9 +1,13 @@
+
+# load client id and secret
+source("my_credentials.R")
+
 # connect to Spotify API
 
 connect_spotify<-function(){
   cat("Connecting with Spotify API")
-  Sys.setenv(SPOTIFY_CLIENT_ID = '4e46be4ec1834ac78e04e98bd2d2d7a2')
-  Sys.setenv(SPOTIFY_CLIENT_SECRET = 'd63823deb0fa495e9f502c4e9fb97b18')
+  Sys.setenv(SPOTIFY_CLIENT_ID = passport[1])
+  Sys.setenv(SPOTIFY_CLIENT_SECRET = passport[2])
   api_token <- get_spotify_access_token()
   return(api_token)
 }
